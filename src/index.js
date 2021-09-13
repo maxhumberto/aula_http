3f1ebc117ec96ec53632d49e879fb49098af5118
razer <humbertolucas.2416@aluno.saojudas.br>
2021-09-13 11:45:41 -0300

    aula de http

---
+++ src/index.js
@@ -0,0 +1,43 @@
+const http = require ('http')
+const express = require ('express')
+const app = express()
+const bodyParser = require ('body-parser')
+app.use(bodyParser.json())
+let id = 3
+let alunos = [
+    {
+        id:1,
+        nome: 'João',
+        fone:'11223344',
+        email:'joao@email.com'
+    },
+    {
+        id:2,
+        nome: 'Maria',
+        fone:'55667788',
+        email:'maria@email.com'
+    }
+]
+
+const porta = 3000
+app.set('port', porta)
+const server = http.createServer(app)
+server.listen(porta)
+// localhots:3000/alunos 
+app.post('/alunos', (req, res) => {
+    id = id + 1
+    const aluno = {
+        id: id,
+        nome: req.body.nome,
+        fone: req.body.fone,
+        email: req.body.email
+    
+    }
+    alunos.push(aluno)
+    res.status(201).json(aluno)
+    
+})
+
+app.get('/alunos', (req, res) =>{
+    res.status(200).json(alunos)
+})
